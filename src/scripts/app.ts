5496ff41269f323740dfd425a9cae37560ba4f2d
filtered <176114999+webfiltered@users.noreply.github.com>
2025-06-07 23:57:29 -0700

    Revert "[fix] Remove dynamic import timing issue causing Playwright test flakiness" (#4102)

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -53,7 +53,6 @@ import type { ComfyExtension, MissingNodeType } from '@/types/comfy'
 import { ExtensionManager } from '@/types/extensionTypes'
 import { ColorAdjustOptions, adjustColor } from '@/utils/colorUtil'
 import { graphToPrompt } from '@/utils/executionUtil'
-import { getFileHandler } from '@/utils/fileHandlers'
 import {
   executeWidgetsCallback,
   fixLinkInputSlots,
@@ -1454,6 +1453,7 @@ export class ComfyApp {
    * @param {File} file
    */
   async handleFile(file: File) {
+    const { getFileHandler } = await import('@/utils/fileHandlers')
     const removeExt = (f: string) => {
       if (!f) return f
       const p = f.lastIndexOf('.')
